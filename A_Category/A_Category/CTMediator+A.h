ec24554caa9293847d913fa5c14e7085a37cdb32
Bing <bing.wu566@gmail.com>
2017-07-28 15:11:51 +0800

    eidt

--- A_Category/A_Category/CTMediator+A.h
+++ A_Category/A_Category/CTMediator+A.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017年 bing. All rights reserved.
 //
 
-#import "CTMediator.h"
+#import <CTMediator/CTMediator.h>
 #import <UIKit/UIKit.h>
 @interface CTMediator (A)
 - (UIViewController *)A_aViewController;
